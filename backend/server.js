9c8df53a545106c0a2d2b2a37d66372d39cf44e9
PRbrate <pauloteles08@hotmail.com>
2026-04-23 11:43:21 -0300

    Feat: Start Server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
 //Application Programming Interface
+// Rest- regras para construção de APIs Escalaveis utilizando métodos
+// JSON - Java Script Object Notation
+import express from "express";
+
+const app = express();
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+
+    res.json({ "Nome": "Paulo", "Sobrenome": "Teles" });
+});
+
+app.post('/', (req, res) => {
+    const teste = req.body;
+    res.json({
+        sucesso: true,
+        messagem: "Dados Recebidos",
+        dados: teste,
+    })
+})
+
+app.listen(3000, () => console.log("O servidor está rodando!! porta: 3000"));
