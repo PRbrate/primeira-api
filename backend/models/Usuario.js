376c16a5aaba0f674dd9a2b2c69c48b1ad6e04fd
PRbrate <pauloteles08@hotmail.com>
2026-04-23 13:04:47 -0300

    Feat: config orm and post user

---
+++ backend/models/Usuario.js
@@ -0,0 +1,7 @@
+export default class Usuario {
+    constructor(nome, email, senha) {
+        this.email = email;
+        this.nome = nome;
+        this.senha = senha;
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,35 @@
 // Rest- regras para construção de APIs Escalaveis utilizando métodos
 // JSON - Java Script Object Notation
 import express from "express";
+import Usuario from "./models/Usuario.js";
+import { PrismaClient } from '@prisma/client'
+
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.use(express.json())
+app.use(express.json());
 
-
-app.get('/', (req, res) => {
-
-    res.json({ "Nome": "Paulo", "Sobrenome": "Teles" });
+app.post("/user", async (req, res) => {
+    try {
+        const usuario = new Usuario(req.body.nome, req.body.email, req.body.senha);
+        const novoUsuario = await prisma.Usuario.create({
+            data: usuario
+        });
+        res.status(201).json(novoUsuario);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-app.post('/', (req, res) => {
-    const teste = req.body;
-    res.json({
-        sucesso: true,
-        messagem: "Dados Recebidos",
-        dados: teste,
-    })
+app.get("/user", async (req, res) => {
+    try {
+        const usuarios = await prisma.usuario.findMany();
+
+        res.status(201).json(usuarios);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 app.listen(3000, () => console.log("O servidor está rodando!! porta: 3000"));
